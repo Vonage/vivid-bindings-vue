d4ee160e65e3384d424fb293b395b2c50f845f50
Alexander Mazuruk <alexander.mazuruk@gmail.com>
2023-02-24 22:02:18 +0100

    finalize auto-update guided by vivid latest version

--- src/vivid.has.new.version/mod.ts
+++ src/vivid.has.new.version/mod.ts
@@ -1,18 +1,16 @@
-import vividPkg from 'https://esm.sh/@vonage/vivid@latest/package.json' assert { type: "json" }
-import { versionFile } from '../consts.ts'
-
 /**
  * Determines if the new version of bindings needs to be generated & auto published as npm package
- *   returns exit code `true` when Vivid has a newer version and there are No corresponding bindings package exists
- *   returns exit code `false` when Vivid latest version equal to the latest bindings package
+ *   returns result code `true` when Vivid has a newer version and there are No corresponding bindings package exists
+ *   returns result code `false` when Vivid latest version equal to the latest bindings package
  */
+
+import latestVividPkg from 'https://esm.sh/@vonage/vivid@latest/package.json' assert { type: "json" }
+import { versionFile } from '../consts.ts'
+
 const { readFileSync } = Deno
 
-const previousVersion = new TextDecoder('utf-8').decode(readFileSync(versionFile))
-const bindingsPkg = { version: previousVersion }
+const bindingsPkg = { version: new TextDecoder('utf-8').decode(readFileSync(versionFile)) }
 
-const versionsAreEquals = bindingsPkg.version === vividPkg.version
+const versionsAreDifferent = bindingsPkg.version !== latestVividPkg.version
 
-// console.info(`Vivid latest version is ${vividPkg.version}\nVueJs bindings latest version is ${bindingsPkg.version}`)
-// console.info(`Resolution: ${!versionsAreEquals ? 'Bindings generation is needed' : 'Idle'}`)
-console.log(`result=${!versionsAreEquals}`)
+console.log(`result=${versionsAreDifferent}`)
--- todo.md
+++ todo.md
@@ -1,5 +1,4 @@
 # publish `@vonage/vivid-bindings-vue` package
-# add storage for current bindings package version, markdown wiki repo for instance
 # resolve fonts delivery dilemma between public / private package
 # handle vivid components events contracts
 # handle Vivid consts enums like ButtonConnotation, ButtonShape, IconConnotation, etc.
