0665b235707289ed5a4c6af23786e66ad92df5f7
Alexander Mazuruk <alexander.mazuruk@gmail.com>
2023-02-24 21:33:26 +0100

    Update mod.ts

--- src/vivid.has.new.version/mod.ts
+++ src/vivid.has.new.version/mod.ts
@@ -3,22 +3,16 @@ import { versionFile } from '../consts.ts'
 
 /**
  * Determines if the new version of bindings needs to be generated & auto published as npm package
- *   returns exit code `1` when Vivid has a newer version and there are No corresponding bindings package exists
- *   returns exit code `0` when Vivid latest version equal to the latest bindings package
+ *   returns exit code `true` when Vivid has a newer version and there are No corresponding bindings package exists
+ *   returns exit code `false` when Vivid latest version equal to the latest bindings package
  */
 const { readFileSync } = Deno
-
-enum ResultCode {
-  generationIsNeeeded = 1,
-  idle = 0
-}
 
 const previousVersion = new TextDecoder('utf-8').decode(readFileSync(versionFile))
 const bindingsPkg = { version: previousVersion }
 
 const versionsAreEquals = bindingsPkg.version === vividPkg.version
-const result = versionsAreEquals ? ResultCode.idle : ResultCode.generationIsNeeeded
 
 console.info(`Vivid latest version is ${vividPkg.version}\nVueJs bindings latest version is ${bindingsPkg.version}`)
-console.info(`Resolution: ${result === ResultCode.generationIsNeeeded ? 'Bindings generation is needed' : 'Idle'}`)
-console.log(`result=${result}`)
+console.info(`Resolution: ${!versionsAreEquals ? 'Bindings generation is needed' : 'Idle'}`)
+console.log(`result=${versionsAreEquals}`)
